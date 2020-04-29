89a0f9d8d2560aa1c80afcecfb08dbf635eec26e
daria <daria.pindus@gmail.com>
2020-04-29 18:02:18 +0300

    add command line check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please, specify one parameter (file or folder name)");
+            return;
+        }
+            
         String filename = args[0];
 
         JavaCodeLinesCounter codeLinesCounter = new JavaCodeLinesCounter(filename);
